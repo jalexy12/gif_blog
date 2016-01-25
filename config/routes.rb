c296822a46a3bb53505caf1e4f9fa782bac89e5e
Josh <jalexy12@gmail.com>
2016-01-25 11:57:14 -0500

    Created posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/show'
+
+  get 'posts/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
